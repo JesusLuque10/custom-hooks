47bd316261c9f8282c7d6a12375420ca173f9adb
Jesus <jlm011094@gmail.com>
2025-01-16 13:51:42 +0100

    add useTodos

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from 'react';
-import { todoReducer } from '../08-useReducer/todoReducer';
+import { todoReducer } from './todoReducer';
 
 //Esto es para en la inicialización recoger lo que hay en el localstorage
 const init = () => {
